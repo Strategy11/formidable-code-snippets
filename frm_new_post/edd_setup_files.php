e15704619d6df4e2dae42d05d9b7ad7f1aeb6b38
Steph <steph@strategy11.com>
2015-04-24 14:41:14 -0600

    Add frm_new_post example

---
+++ frm_new_post/edd_setup_files.php
@@ -0,0 +1,29 @@
+<?php
+
+add_filter('frm_new_post', 'edd_setup_files', 10, 2);
+function edd_setup_files($post, $args) {
+	if ( $args['form']->id != 5 ) { //change 5 to the ID of your form
+		return;
+	}
+
+	global $frm_vars;
+	// don't continue if no files were uploaded
+	if( ! isset( $frm_vars['media_id'] ) || empty( $frm_vars['media_id'] ) ) {
+		return;
+	}
+
+	$edd_files = array();
+	foreach ( (array) $frm_vars['media_id'] as $media_id ) {
+		foreach ( (array) $media_id as $id ) {
+			$attachment = get_post( $media_id );
+			$edd_files[] = array(
+				'file'      => wp_get_attachment_url( $id ),
+				'condition' => '',
+				'name'      => basename( $attachment->guid ), // change this line to get the name you want
+			);
+		}
+	}
+
+	$post['post_custom']['edd_download_files'] = $edd_files;
+	return $post;
+}
